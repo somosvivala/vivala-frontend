cdb0a546da82a3c5faf96117bc7d224ce04f548e
emanuellarini <emanuellarini@hotmail.com>
2017-07-24 03:29:35 -0300

    [fix[ revert this

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
-if (process.env.NODE_ENV === 'production') {
-    axios.defaults.baseURL = 'http://52.67.252.83/api';
-} else {
-    axios.defaults.baseURL = 'http://vivala.dev/api';
+axios.defaults.baseURL = 'http://52.67.252.83/api';
+
+// if (process.env.NODE_ENV === 'production') {
+//     axios.defaults.baseURL = 'http://52.67.252.83/api';
+// } else {
+//     axios.defaults.baseURL = 'http://vivala.dev/api';
 }
 
 export default axios;
