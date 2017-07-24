0c0e5d1660b1f221fc72dbaf2b4bc67f5d0a901d
emanuellarini <emanuellarini@hotmail.com>
2017-07-24 03:33:45 -0300

    [add] use production url

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-if (process.env.NODE_ENV === 'production') {
+// if (process.env.NODE_ENV === 'production') {
     axios.defaults.baseURL = 'http://52.67.252.83/api';
-} else {
-    axios.defaults.baseURL = 'http://vivala.dev/api';
-}
+// } else {
+//     axios.defaults.baseURL = 'http://vivala.dev/api';
+// }
 
 export default axios;
