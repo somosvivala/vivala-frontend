673b5c3c0635d545c671d34c57e3db83d23a15a2
emanuellarini <emanuellarini@hotmail.com>
2017-07-24 03:32:06 -0300

    Revert "[fix[ revert this"
    This reverts commit cdb0a546da82a3c5faf96117bc7d224ce04f548e.

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,11 +1,9 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://52.67.252.83/api';
-
-// if (process.env.NODE_ENV === 'production') {
-//     axios.defaults.baseURL = 'http://52.67.252.83/api';
-// } else {
-//     axios.defaults.baseURL = 'http://vivala.dev/api';
+if (process.env.NODE_ENV === 'production') {
+    axios.defaults.baseURL = 'http://52.67.252.83/api';
+} else {
+    axios.defaults.baseURL = 'http://vivala.dev/api';
 }
 
 export default axios;
