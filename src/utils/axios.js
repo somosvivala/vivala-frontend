400d3d7afa1a3cd4c383d55d2782338e5d4ff423
Renato Gomes <renatoo.agomes@gmail.com>
2017-07-28 15:44:28 -0300

    Commit com URL de produção

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // if (process.env.NODE_ENV === 'production') {
-    axios.defaults.baseURL = 'http://52.67.252.83/api';
+    axios.defaults.baseURL = 'http://api.vivala.com.br/api';
 // } else {
 //     axios.defaults.baseURL = 'http://vivala.dev/api';
 // }
