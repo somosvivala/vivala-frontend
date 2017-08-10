816a51f6c8a39ddb4b936c57bdb7730ccdd21ec1
Renato Gomes <renatoo.agomes@gmail.com>
2017-08-10 01:12:24 -0300

    commit com mudança ssl link api

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // if (process.env.NODE_ENV === 'production') {
-    axios.defaults.baseURL = 'http://api.vivala.com.br/api';
+    axios.defaults.baseURL = 'https://api.vivala.com.br/api';
 // } else {
 //     axios.defaults.baseURL = 'http://vivala.dev/api';
 // }
