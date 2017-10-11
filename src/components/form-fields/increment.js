753c01c1cfb62a0e1b371c9e9f6192f8074dace4
emanuellarini <emanuellarini@hotmail.com>
2017-10-11 02:16:25 -0300

    [add] error visualisation to increment form field

--- src/components/form-fields/increment.js
+++ src/components/form-fields/increment.js
@@ -4,28 +4,31 @@ import Typography from 'material-ui/Typography';
 
 class Increment extends Component {
     render() {
-        const { title, helper, input } = this.props;
+        const { title, helper, input, meta: {error} } = this.props;
         return (
-            <div style={{display: 'flex', justifyContent: 'space-between', width: '100%'}}>
-                <div>
-                    <Typography type="subheading" color="inherit" style={{ textTransform: 'uppercase' }}>{title}</Typography>
-                    <Typography type="body1">{helper}</Typography>
+            <div>
+                <div style={{display: 'flex', justifyContent: 'space-between', width: '100%'}}>
+                    <div>
+                        <Typography type="subheading" color="inherit" style={{ textTransform: 'uppercase' }}>{title}</Typography>
+                        <Typography type="body1">{helper}</Typography>
+                    </div>
+                    <div style={{ display: 'flex', alignItems: 'center' }}>
+                        <Button raised
+                                dense
+                                onClick={(event, index, value) => input.onChange(parseInt(input.value, 10)-1)}
+                        >
+                            -
+                        </Button>
+                        <div style={{ backgroundColor: 'white', width: 25, height: 25, lineHeight: '25px', textAlign: 'center' }}>{input.value}</div>
+                        <Button raised
+                                dense
+                                onClick={(event, index, value) => input.onChange(parseInt(input.value, 10)+1)}
+                        >
+                            +
+                        </Button>
+                    </div>
                 </div>
-                <div style={{ display: 'flex' }}>
-                    <Button raised
-                            dense
-                            onClick={(event, index, value) => input.onChange(parseInt(input.value, 10)-1)}
-                    >
-                        -
-                    </Button>
-                    <div style={{ backgroundColor: 'white', width: 25, height: 25, lineHeight: '25px', textAlign: 'center' }}>{input.value}</div>
-                    <Button raised
-                            dense
-                            onClick={(event, index, value) => input.onChange(parseInt(input.value, 10)+1)}
-                    >
-                        +
-                    </Button>
-                </div>
+                { !!error && <Typography type="caption" style={{ color: 'red', marginTop: 10 }}>{error}</Typography> }
             </div>
         );
     }
