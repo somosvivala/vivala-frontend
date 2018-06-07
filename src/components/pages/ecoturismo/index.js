fb22081aff3dc57cd2060975c84c365dd9d95038
Renato Gomes <renatoo.agomes@gmail.com>
2018-06-07 18:07:14 -0300

    trocando nos outros 'index'

--- src/components/pages/ecoturismo/index.js
+++ src/components/pages/ecoturismo/index.js
@@ -39,7 +39,7 @@ class EcoturismoIndex extends Component {
                     <meta property="og:site_name" content="Vivalá" />
                     <meta property="og:description" content="Viaje pelo Brasil com roteiros de Turismo e Voluntariado, Experiências de Conexão com a Natureza e Lugares Incríveis para ir quando quiser." />
                     <meta property="og:type" content="website" />
-                    <meta property="og:image" content="https://res.cloudinary.com/vivala/image/upload/imagem_home_1524934756" />
+                    <meta property="og:image" content="https://res.cloudinary.com/vivala/image/upload/v1528403873/home_volunturismo_1527197447_vasb9e.jpg" />
                     <meta property="og:image:type" content="image/jpg" />
                     <meta property="og:image:width" content="1200" />
                     <meta property="og:image:height" content="630" />
--- src/components/pages/index/index.js
+++ src/components/pages/index/index.js
@@ -45,10 +45,10 @@ class IndexPage extends Component {
                     <meta property="og:site_name" content="Vivalá" />
                     <meta property="og:description" content="Viaje pelo Brasil com roteiros de Turismo e Voluntariado, Experiências de Conexão com a Natureza e Lugares Incríveis para ir quando quiser." />
                     <meta property="og:type" content="website" />
-                    <meta property="og:image" content="https://res.cloudinary.com/vivala/image/upload/imagem_home_1524934756" />
+                    <meta property="og:image" content="https://res.cloudinary.com/vivala/image/upload/v1528403873/home_volunturismo_1527197447_vasb9e.jpg" />
                     <meta property="og:image:type" content="image/jpg" />
-                    <meta property="og:image:width" content="1200" />
-                    <meta property="og:image:height" content="630" />
+                    <meta property="og:image:width" content="1621" />
+                    <meta property="og:image:height" content="788" />
                 </Helmet>
                 <Servicos fotoVolunturismo={fotoVolunturismo} fotoEcoturismo={fotoEcoturismo} fotoImersoes={fotoImersoes} />
                 <Social color="#aaa"
